3be0c7f61026d83da9142ee7d30f41bc1522a9f1
IP <92016251+IsmenioPaulo@users.noreply.github.com>
2024-04-17 13:41:20 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Izidine, you are a great person ma G, everybody around you loves you dude</h1>
+  return <h1>Izidine Ambasse, you are a great person my G, we did not use to talk much when we were in secondary school, but we got close in the last 5y and our connection is fucking great, know that everybody around you loves you dude. ONE LOVE</h1>
 }
 
 
