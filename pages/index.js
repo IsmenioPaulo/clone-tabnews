e5c7ff7a081a891bf348019010bb33f36637fe87
IP <92016251+IsmenioPaulo@users.noreply.github.com>
2024-04-24 12:39:54 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,27 @@
 function Home() {
-
-  return <h1>Izidine Ambasse, you are a great person my G, we did not use to talk much when we were in secondary school, but we got close in the last 5y and our connection is fucking great, know that everybody around you loves you dude. ONE LOVE YOUNG KING</h1>
-
+  return (
+    <h1>
+      Izidine Ambasse, you are a great person my G, we did not use to talk much
+      when we were in secondary school, but we got close in the last 5y and our
+      connection is fucking great, know that everybody around you loves you
+      dude. ONE LOVE YOUNG KING
+    </h1>
+  );
 }
 
+function HomeM() {
+  return (
+    <h1>
+      Izidine Ambasse, you are a great person my G, we did not use to talk much
+      when we were in secondary school, but we got close in the last 5y and our
+      connection is fucking great, know that everybody around you loves you
+      dude. ONE LOVE YOUNG KING
+    </h1>
+  );
+}
+
+function teste2() {
+  console.log("Indentacao codigo");
+}
 
 export default Home;
